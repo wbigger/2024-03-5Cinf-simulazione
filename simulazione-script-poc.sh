410b6f00341522918b5f89c77710b66b01af2010
Claudio <wbigger@gmail.com>
2024-03-15 22:21:35 +0100

    Fix github download

--- simulazione-script-poc.sh
+++ simulazione-script-poc.sh
@@ -3,6 +3,7 @@ sudo apt update -y
 sudo apt install nodejs unzip wget npm mysql-server git -y
 cd /home/ubuntu
 git clone https://github.com/wbigger/2024-03-5Cinf-simulazione
+mv 2024-03-5Cinf-simulazione/resources/ .
 cd resources/codebase_partner
 npm install aws aws-sdk
 sudo mysql -u root -e "CREATE USER 'nodeapp' IDENTIFIED WITH mysql_native_password BY 'student12'";
@@ -17,12 +18,7 @@ sudo mysql -u root -e "USE ORDERS; CREATE TABLE orders(
 sudo sed -i 's/.*bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl enable mysql
 sudo service mysql restart
-export APP_DB_HOST=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-export APP_DB_USER=nodeapp
-export APP_DB_PASSWORD=student12
-export APP_DB_NAME=ORDERS
-export APP_PORT=8080
-sudo -E npm start &
+sudo APP_PORT=8080 APP_DB_HOST=localhost APP_DB_USER=nodeapp APP_DB_PASSWORD=student12 APP_DB_NAME=ORDERS npm start &
 echo '#!/bin/bash -xe
 cd /home/ubuntu/resources/codebase_partner
 export APP_PORT=8080
